08f85a8cd2a5c713ac74e4f4c540a7c78aa2b66b
Michel <michelbreno5@hotmail.com>
2024-02-16 17:16:58 -0300

    feat(Person and Expense): Adjusting relations to better fit the purpose of the model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,4 @@
 class Expense < ApplicationRecord
-  belongs_to :paid_by, class_name: 'Person'
-  has_many :people, through: :paid_by
-  has_many :people, through: :paid_for
+  belongs_to :paid_by, class_name: 'Person', foreign_key: 'paid_by_id'
+  belongs_to :paid_for, class_name: 'Person', foreign_key: 'paid_for_id'
 end
