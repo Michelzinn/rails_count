50e3367c7dd001e42d6c06f6ef8b3b551b750350
Michel <michelbreno5@hotmail.com>
2024-02-17 15:43:41 -0300

    refactor(Person): Removing expenses from person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,3 @@
 class Person < ApplicationRecord
   belongs_to :group
-  has_many :expenses_paid, class_name: 'Expense', foreign_key: 'paid_by_id'
-  has_many :expenses_paid_for, class_name: 'Expense', foreign_key: 'paid_for_id'
 end
